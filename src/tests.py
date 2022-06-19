4427ef0e0316ae4033b5f74f8fe9fe211b3cce60
Basil Eagle <basileagle@gmail.com>
2022-06-19 18:47:47 +0100

    added requirements and pytest

--- src/tests.py
+++ src/tests.py
@@ -1,13 +1,9 @@
 import game
 
-def game_resource_tests():
-    tile = game.Tile("Hills", 3, [])
-    assert tile.check_proc(5) is None
-    assert isinstance(tile.check_proc(3), game.Resource)
-    assert str(tile.check_proc(3)) == "Brick"
+class TestClass:
+    def test_game_resources(self):
+        tile = game.Tile("Hills", 3, [])
+        assert tile.check_proc(5) is None
+        assert isinstance(tile.check_proc(3), game.Resource)
+        assert str(tile.check_proc(3)) == "Brick"
 
-def main():
-    game_resource_tests()
-
-if __name__ == '__main__':
-    main()
