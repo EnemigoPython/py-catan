d79b349a8a4c88e908002aa1995c81dbc19724d5
Basil Eagle <basileagle@gmail.com>
2022-06-19 19:17:34 +0100

    changed resource str to repr

--- src/game.py
+++ src/game.py
@@ -1,19 +1,21 @@
+from typing import List, Dict, Self
+
 class InvalidResourceException(Exception):
     """Custom error handler for creation of unidentified resource"""
 
 class Resource:
-    def __init__(self, name):
+    def __init__(self, name: str):
         if name in ("Brick", "Lumber", "Ore", "Grain", "Wool"):
             self.name = name
         else:
             raise InvalidResourceException(f"{name} is not a resource")
 
-    def __str__(self):
+    def __repr__(self):
         return self.name
 
 class Tile:
     """A single tile on the Catan game board"""
-    resource_dict = {
+    resource_dict: Dict[str, Resource | None] = {
         "Desert": None,
         "Hills": Resource("Brick"),
         "Forest": Resource("Lumber"),
@@ -21,14 +23,16 @@ class Tile:
         "Fields": Resource("Grain"),
         "Pasture": Resource("Wool")
     }
-    
-    def __init__(self, terrain, number, neighbours):
+
+    def __init__(self, terrain: str, number: int, neighbours: List[Self]):
         self.terrain = terrain
         self.number = number
         self.neighbours = neighbours
         self.resource = self.resource_dict[self.terrain]
+        self.construction_slots: List[Construction]
+        self.road_slots: List[Road]
 
-    def check_proc(self, number):
+    def check_proc(self, number: int):
         if self.number == number:
             return self.resource_dict[self.terrain]
         return None
@@ -36,5 +40,8 @@ class Tile:
 class Construction:
     """An item constructed by a player"""
     pass
+
+class Road:
+    """A road placed by a player"""
 
 # x = Resource("hi")
