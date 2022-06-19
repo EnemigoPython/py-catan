9de9b720940753f690a83094934c2c7e5bbb060a
Basil Eagle <basileagle@gmail.com>
2022-06-19 18:37:16 +0100

    added testing file

--- src/tests.py
+++ src/tests.py
@@ -1,9 +1,13 @@
 import game
 
-def main():
+def game_resource_tests():
     tile = game.Tile("Hills", 3, [])
     assert tile.check_proc(5) is None
-    assert tile.check_proc(3) == "Brick", tile.check_proc(3)
+    assert isinstance(tile.check_proc(3), game.Resource)
+    assert str(tile.check_proc(3)) == "Brick"
+
+def main():
+    game_resource_tests()
 
 if __name__ == '__main__':
     main()
