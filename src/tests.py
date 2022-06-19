58832ca5fd70d17d7b992c15d53e846cc7d9ddee
Basil Eagle <basileagle@gmail.com>
2022-06-19 18:55:45 +0100

    remove workflow error test

--- src/tests.py
+++ src/tests.py
@@ -6,5 +6,3 @@ class TestClass:
         assert tile.check_proc(5) is None
         assert isinstance(tile.check_proc(3), game.Resource)
         assert str(tile.check_proc(3)) == "Brick"
-        assert 3 == 5
-
