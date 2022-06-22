2008b7bd575c14a38e8e824864ef5bf271ad2db1
Basil Eagle <basileagle@gmail.com>
2022-06-22 22:35:19 +0100

    stop myself from overengineering resources

--- src/game.py
+++ src/game.py
@@ -3,28 +3,18 @@ from typing import List, Dict, TypeVar
 class InvalidResourceException(Exception):
     """Custom error handler for creation of unidentified resource"""
 
-class Resource:
-    def __init__(self, name: str):
-        if name in ("Brick", "Lumber", "Ore", "Grain", "Wool"):
-            self.name = name
-        else:
-            raise InvalidResourceException(f"{name} is not a resource")
-
-    def __repr__(self):
-        return self.name
-
 class Tile:
     """A single tile on the Catan game board"""
 
     Self = TypeVar("Self", bound="Tile")
     
-    resource_dict: Dict[str, Resource | None] = {
+    resource_dict: Dict[str, str | None] = {
         "Desert": None,
-        "Hills": Resource("Brick"),
-        "Forest": Resource("Lumber"),
-        "Mountains": Resource("Ore"),
-        "Fields": Resource("Grain"),
-        "Pasture": Resource("Wool")
+        "Hills": "Brick",
+        "Forest": "Lumber",
+        "Mountains": "Ore",
+        "Fields": "Grain",
+        "Pasture": "Wool"
     }
 
     def __init__(self, terrain: str, number: int, neighbours: List[Self]):
@@ -33,18 +23,21 @@ class Tile:
         self.neighbours = neighbours
         self.resource = self.resource_dict[self.terrain]
         self.construction_slots: List[Construction]
-        self.road_slots: List[Road]
+        self.road_slots: List[Construction]
 
     def check_proc(self, number: int):
         if self.number == number:
-            return self.resource_dict[self.terrain]
+            return self.resource
         return None
 
 class Construction:
     """An item constructed by a player"""
-    pass
 
-class Road:
-    """A road placed by a player"""
+    # construction_dict = {
+    #     "Road": [
+    #         Tile.resource_dict 
+    #     ]
+    # }
 
-# x = Resource("hi")
+    # def __init__(self, name: str):
+    #     if name in ("Road", "Settlement")
--- src/tests.py
+++ src/tests.py
@@ -4,5 +4,4 @@ class TestClass:
     def test_game_resources(self):
         tile = game.Tile("Hills", 3, [])
         assert tile.check_proc(5) is None
-        assert isinstance(tile.check_proc(3), game.Resource)
-        assert str(tile.check_proc(3)) == "Brick"
+        assert tile.check_proc(3) == "Brick"
