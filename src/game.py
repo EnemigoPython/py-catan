cff6722e50a7fbb308c317ed424130ae86dab7f7
Basil Eagle <basileagle@gmail.com>
2022-06-24 13:13:19 +0100

    class linking and first naive slots impl

--- src/game.py
+++ src/game.py
@@ -1,5 +1,5 @@
 from enum import Enum, auto
-from typing import List, Dict, TypeVar
+from typing import List, Dict, TypeVar, Union
 
 class Resource(Enum):
     """Used by players to build construction items"""
@@ -15,6 +15,12 @@ class Player:
     def __init__(self):
         self.resources: List[Resource] = []
         self.development_cards: List[DevelopmentCard] = []
+        self.controlled_tiles: List[Tile] = []
+        self.victory_points = 0
+
+    @property
+    def constructions(self):
+        return [item for tile in self.controlled_tiles for item in tile.construction_slots if item.owner is self]
 
 class Harbour:
     """A trading port that can be used for better deals"""
@@ -34,14 +40,14 @@ class Tile:
         "Pasture": Resource.Wool
     }
 
-    def __init__(self, terrain: str, number: int, neighbours: List[Self]):
+    def __init__(self, terrain: str, number: int, neighbours: List[Self]=[]):
         self.terrain = terrain
         self.number = number
         self.neighbours = neighbours
         self.resource = self.resource_dict[self.terrain]
         self.construction_slots: List[Construction] = []
         self.road_slots: List[Road] = []
-        self.harbour_slot: Harbour | None = None
+        self.harbour_slot: Union[Harbour, None] = None
 
     def check_proc(self, number: int):
         return self.resource if number == self.number else None
@@ -75,9 +81,10 @@ class Construction:
     def can_build(item: str, player: Player):
         return all(player.resources.count(key) >= val for key, val in Construction.construction_dict[item].items())
 
-    def __init__(self, name: str):
+    def __init__(self, name: str, owner: Player):
         assert name in self.construction_dict.keys()
         self.name = name
+        self.owner = owner
 
     def __repr__(self):
         return self.name
@@ -85,8 +92,35 @@ class Construction:
 class Road(Construction):
     """A road to put your wagon on"""
 
-    def __init__(self):
-        super().__init__("Road")
+    def __init__(self, owner: Player, tile: Tile):
+        self.tile = tile
+        self.tile.road_slots.append(self)
+        super().__init__("Road", owner)
+
+class SettlementOrCity(Construction):
+    """Hybrid class for settlements/cities"""
+
+    def __init__(self, owner: Player, tile: Tile):
+        self.tile = tile
+        self.tile.construction_slots.append(self)
+        super().__init__("Settlement", owner)
+
+    def upgrade_to_city(self):
+        self.name = "City"
+        self.owner.victory_points += 1
 
 class DevelopmentCard(Construction):
     """Mystery card to give players an edge"""
+
+    def __init__(self, owner: Player):
+        super().__init__("Development Card", owner)
+
+
+player = Player()
+player2 = Player()
+tile = Tile("Hills", 3)
+settlement = SettlementOrCity(player, tile)
+settlement2 = SettlementOrCity(player2, tile)
+settlement3 = SettlementOrCity(player, tile)
+player.controlled_tiles.append(tile)
+print(player.constructions)
